4ce1369bfc4070601a0fd2c918b9b7193a1c1123
Alanmenga <alan.gabriel.mengassini@gmail.com>
2021-04-06 20:14:01 -0300

    Ejercicio 20

--- app/Clase3/archivos.php
+++ app/Clase3/archivos.php
@@ -1,31 +1,5 @@
 <?php
 
-function EscribirArchivoJSON($file_name, $Obj){
-    if (!file_exists($file_name) || filesize($file_name) == 0){
-        $file = fopen($file_name, "w");
-        fwrite($file, json_encode($Obj));
-        fclose($file);
-    }
-    else{
-        $file = fopen($file_name, "c");
-        fseek($file, filesize($file_name)-1);        
-        fwrite($file, ",".json_encode($Obj)."]");        
-        fclose($file);
-    }
-}
-
-function LeerArchivoJSON($file_name)
-{
-    $array = [];    
-    if (file_exists($file_name) && filesize($file_name) > 0){
-        $file = fopen($file_name, "r");
-        $contenido = fread($file, filesize($file_name));
-        $array = json_decode($contenido);
-        fclose($file);
-    }    
-    return $array;
-}
-
 
 function EscribirArchivoTxt($file_name, $msg){    
     $pudo = 0;
